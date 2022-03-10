9e347e375517b701149dbfc19bd1a91bd007fc42
life-random <80086175+life-random@users.noreply.github.com>
2022-03-10 12:17:59 +0900

    Add files via upload

---
+++ 20220310/DataType.java
@@ -0,0 +1,6 @@
+
+public class DataType {
+	public static void main(String[] args) {
+		System.out.println("Java Data Type - Primitive");
+	}
+}
---
+++ 20220310/InputScn.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class InputScn {
+	public static void main(String[] args) {
+		int a;
+		String name;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		a = sc.nextInt();
+		
+		System.out.print("이름 입력 : ");
+		name = sc.next();
+		
+		System.out.println(a);
+		System.out.println(name);
+		
+		sc.close();
+	}
+}
